80fb5d339f6b09d116276908bccaadeac7e56fb7
Sahil Singh Kushwaha <sahilsinghkushwaha6@gmail.com>
2026-05-14 22:13:55 +0530

    Delete evenOrOdd.py

--- evenOrOdd.py
+++
@@ -1,8 +0,0 @@
-# check whether a number is even or odd
-
-num = int(input("Enter a number: "))
-
-if num % 2 == 0:
-    print(num, "is an even number.")
-else:    
-    print(num, "is an odd number.")
