08a4c12faf86f92e76d94047a06b4354ed38e97f
Sahil Singh Kushwaha <sahilsinghkushwaha6@gmail.com>
2026-05-14 22:08:15 +0530

    Delete checkDivisibility.py

--- checkDivisibility.py
+++
@@ -1,9 +0,0 @@
-# print numbers from 1 to 100 which is divisible by 3, 5 and both
-
-for i in range(1, 101):
-    if i % 3 == 0:
-        print(i, " is divisible by 3.")
-    if i % 5 == 0:
-        print(i, " is divisible by 5.")
-    if i % 3 == 0 and i % 5 == 0:
-        print(i, " is divisible by 3 and 5 both.")
